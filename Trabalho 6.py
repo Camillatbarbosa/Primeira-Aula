df74c5f3fca131eb0329ae3c14d1ff2628b0be3e
Camillatbarbosa <159266551+Camillatbarbosa@users.noreply.github.com>
2024-03-20 10:16:27 -0300

    Add files via upload

---
+++ Trabalho 6.py
@@ -0,0 +1,32 @@
+print("Camilla")
+
+numero = 10
+nome = "Camilla"
+booleano = True
+
+numero1 = 5
+numero2 = 10
+numero3 = 15
+numero4 = 20
+frase = "Olá! Meu nome é Camilla e estou cursando Direito na faculdade"
+palavra = len(nome)
+maior = max (numero1, numero2, numero3, numero4)
+
+media = (numero1 + numero2 + numero3 + numero4) /4
+print("A média dos números é:", media)
+
+quadrado = (numero1 * numero1)
+print("O quadrado de 5 é:", quadrado)
+
+dobro = (numero2 * 2)
+print("O dobro de 10 é:", dobro)
+
+print("Quantidade de letras na palavra Camilla:", palavra)
+
+qtd = frase.count(' ')
+print("A quantidade de espaços na frase: Olá! Meu nome é Camilla e estou cursando Direito na faculdade, é:", qtd)
+
+print("O primeiro é maior que o segundo?", numero1 > numero2)
+
+max (numero1, numero2, numero3, numero4)
+print(maior) 
